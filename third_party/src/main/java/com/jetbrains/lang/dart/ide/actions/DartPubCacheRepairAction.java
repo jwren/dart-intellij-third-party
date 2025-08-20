24cecc7c90d36f6895086912c49e620e7b07afa3
Jaime Wren <jwren@google.com>
2025-08-20 15:00:24 -0700

    [cq] Migrate off of deprecated instance of showOkCancelDialog (#56)

--- third_party/src/main/java/com/jetbrains/lang/dart/ide/actions/DartPubCacheRepairAction.java
+++ third_party/src/main/java/com/jetbrains/lang/dart/ide/actions/DartPubCacheRepairAction.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.jetbrains.lang.dart.ide.actions;
 
+import com.intellij.CommonBundle;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.NlsContexts;
@@ -15,10 +16,11 @@ public class DartPubCacheRepairAction extends DartPubActionBase {
     return DartBundle.message("dart.pub.cache.repair.title");
   }
 
-  @Override
-  protected String @Nullable [] calculatePubParameters(final @NotNull Project project, final @NotNull VirtualFile pubspecYamlFile) {
-    final int choice = Messages.showOkCancelDialog(project, DartBundle.message("dart.pub.cache.repair.message"),
-                                                   DartBundle.message("dart.pub.cache.repair.title"), Messages.getWarningIcon());
-    return choice == Messages.OK ? new String[]{"cache", "repair"} : null;
-  }
+    @Override
+    protected String @Nullable [] calculatePubParameters(final @NotNull Project project, final @NotNull VirtualFile pubspecYamlFile) {
+        final int choice = Messages.showOkCancelDialog(project, DartBundle.message("dart.pub.cache.repair.message"),
+                DartBundle.message("dart.pub.cache.repair.title"), CommonBundle.getYesButtonText(),
+                CommonBundle.getNoButtonText(), Messages.getWarningIcon());
+        return choice == Messages.OK ? new String[]{"cache", "repair"} : null;
+    }
 }
